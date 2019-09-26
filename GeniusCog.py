e37af0d08c6287026b15fac43ca26a3f6689f0c1
KaraMarah <16844293+KaraMarah@users.noreply.github.com>
2019-09-26 10:01:42 -0400

    Update GeniusCog.py
    Added song and lyric search functions

--- GeniusCog.py
+++ GeniusCog.py
@@ -3,8 +3,7 @@ from redbot.core import commands
 from redbot.core import checks
 from redbot.core.utils import chat_formatting as chat
 from redbot.core import Config
-import requests
-import rauth
+import lyricsgenius
 
 class GeniusCog(commands.Cog):
     """Fetches lyrics from Genius"""
@@ -15,13 +14,19 @@ class GeniusCog(commands.Cog):
         message = (
             "To get Genius API info:\n"
             "1. Create an API client at [Genius Developers](https://genius.com/developers)\n"
-            "2. Copy your Client ID and Client secret.\n"
-            "3. Use `{}set api genius clientid,<clientid_here> secret,<secret_here>`"
+            "2. Generate a new access token.\n"
+            "3. Copy your access token.\n"
+            "3. Use `{}set api genius access_token,<token_here>`"
         ).format(ctx.prefix)
         await ctx.maybe_send_embed(message)
 
-    # @commands.command()
-    # @commands.guild_only()
-    # async def genius(self, ctx, *, song: str):
-    #     """Grabs the lyrics for a song from Genius"""
-    #     apikeys = await self.bot.db.api_tokens.get_raw("genius", default={"clientid": none, "secret": none})
+    @commands.command()
+    async def genius(self, ctx, *title: str, *artist: str):
+        geniusToken = await self.bot.db.api_tokens.get_raw("genius", default={"access_token": None})
+        if geniusToken["access_token"] is None:
+            return await ctx.send("The Genius access token has not been set.")
+        genius = lyricsgenius.Genius(geniusToken["access_token"])
+        genius.skip_non_songs = True
+        genius.remove_section_headers = True
+        song = genius.search_song(title, artist)
+        await ctx.send(song.lyrics)
