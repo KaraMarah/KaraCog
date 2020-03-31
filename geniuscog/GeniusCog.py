d867158c323d818d312d79d6d41921d9438aacd8
KaraMarah <karajgalvan@gmail.com>
2020-03-31 19:09:20 -0400

    Update GeniusCog.py

--- geniuscog/GeniusCog.py
+++ geniuscog/GeniusCog.py
@@ -31,6 +31,7 @@ class GeniusCog(commands.Cog):
         geniusToken = await self.bot.get_shared_api_tokens("genius")
         if geniusToken.get("access_token") is None:
             return await ctx.send("The Genius access token has not been set. Use `?geniusapi` for help.")
+        else:
             genius = lyricsgenius.Genius(geniusToken["access_token"])
             genius.skip_non_songs = True
             genius.remove_section_headers = False
@@ -40,4 +41,3 @@ class GeniusCog(commands.Cog):
                     await ctx.send(page)
                 else:
                     await ctx.send(song.lyrics)
-                    
