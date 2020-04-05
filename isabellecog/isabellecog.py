ff6942c798612e870c5db5a47d948432a18aac7d
KaraMarah <karajgalvan@gmail.com>
2020-04-05 15:05:12 -0400

    Update isabellecog.py

--- isabellecog/isabellecog.py
+++ isabellecog/isabellecog.py
@@ -12,4 +12,4 @@ class isabellecog(commands.Cog):
 
     @commands.command()
     async def i(self, ctx, action: str, object: str):
-        await ctx.send("I " + action + object + " too, {}!").format(ctx.author)
+        await ctx.send(f"I {action} {object} too, {ctx.author}!")
