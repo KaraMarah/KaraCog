ee78b19059e46146cf0c82b18a2cd2ed1aec6528
KaraMarah <reversedoblivion@gmail.com>
2023-11-14 00:12:23 -0500

    Update basecog.py

--- basecog/basecog.py
+++ basecog/basecog.py
@@ -1,4 +1,10 @@
+import discord
+import string
+import asyncio
 from redbot.core import commands
+from redbot.core import checks
+from redbot.core.utils import chat_formatting as chat
+from redbot.core.utils.chat_formatting import pagify
 
 class BaseCog(commands.Cog):
     """Custom commands for Isabelle Bot"""
@@ -10,3 +16,11 @@ class BaseCog(commands.Cog):
     async def easypeasy(self, ctx):
         """This command does things"""
         await ctx.send("This is a command!")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        ctx = await self.bot.get_context(message)
+        is_self = ctx.author == ctx.bot.user
+        if not is_self:
+            if ctx.author == 830225163722293258:
+                ctx.react
